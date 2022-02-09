f94e00b824e952a5638645cf3432d15447c6711f
Carlos Ortiz <aldairortizsuarez@gmail.com>
2022-02-09 01:43:45 -0600

    Several files are added and modified

--- Scripts/ejemplo_ploteo_dispercion.R
+++ Scripts/ejemplo_ploteo_dispercion.R
@@ -1,5 +1,99 @@
+install.packages("ggplot2")
+install.packages("ploty")
+library("ggplot2")
+
 #EDA Scatter plot mtcars
 plot(mtcars$mpg ~ mtcars$cyl, xlab="cilindros", ylab= "millas  por galon ",
      main="relacion cilindros y millas por galon")
 plot(mtcars$mpg ~ mtcars$hp, xlab="caballos de fuerza", ylab= "millas  por galon ",
      main="relacion caballos de fuerza y millas por galon")
+
+#Histograma
+
+qplot(mtcars$hp, geom="histogram", 
+      xlab="caballos de fuerza",
+      main="Carros segun caballos de fuerza")
+
+
+
+ggplot(mtcars, aes(x=hp))+ geom_histogram()+ labs(x= 
+      "Caballos de fuerza",y="cantidad de carros",
+       title = "Caballos de fuerza en carros seleccionados"
+      )+ theme(legend.position = "none", panel.background = element_blank(),
+      panel.grid.major = element_blank(), panel.grid = element_blank())
+
+
+ggplot(mtcars, aes(x=hp))+ geom_histogram(binwidth = 30)+ labs(x= 
+       "Caballos de fuerza",y="cantidad de carros",
+          title = "Caballos de fuerza en carros seleccionados"
+)+ theme(legend.position = "none", panel.background = element_blank(),
+         panel.grid.major = element_blank(), panel.grid = element_blank())
+
+
+ggplot()+ geom_histogram(data= mtcars,aes(x=hp), fill="blue", color="red",
+binwidth =20) +labs(x="caballos de fuerza", y="Cantidad de carros",
+title="Caballos de fuerza en carros seleccionados")+xlim(c(80 ,280 ))+
+theme(legend.position = "none")+theme(panel.background = element_blank(), 
+panel.grid.major = element_blank (),
+        panel.grid.minor = element_blank ())
+
+
+#boxplot
+
+boxplot(mtcars$hp, ylab="caballos de fuerza",
+        main="Caballos de fuerza  en carros mtcars" )
+
+
+ggplot(mtcars, aes(x= cyl, y=hp, fill=cyl))+geom_boxplot()+ labs(x="cilindros",
+y= "caballos de fuerza", title="Caballos de fuerza según
+          cilindros de mtcars")+theme(panel.background = element_blank(), 
+        panel.grid.major = element_blank (),
+        panel.grid.minor = element_blank ())
+
+
+#Warning message:Continuous x aesthetic -- did you forget aes(group=...)?
+
+ggplot(mtcars, aes(x=as.factor(cyl), y=hp, fill=cyl))+
+  geom_boxplot(alpha=0.6)+ 
+  labs(x="cilindros",
+          y= "caballos de fuerza", title="Caballos de fuerza según
+          cilindros de mtcars")+theme(panel.background = element_blank(), 
+                                      panel.grid.major = element_blank (),
+                                      panel.grid.minor = element_blank ())
+  
+#otro ejemlp
+#fill simempre se llena con lo que este en x 
+ggplot(mtcars, aes(x=am, y=mpg, fill= am))+
+  geom_boxplot()+
+  labs(x="tipo de cajas",y="millas por galon",
+       title="Millas por galón según tipo de caja-mtcars")+
+  theme(panel.background = element_blank(), 
+        panel.grid.major = element_blank (),
+        panel.grid.minor = element_blank ())
+
+
+#Cambios en las variables
+
+mtcars$am <- factor(mtcars$am, levels = c(T,F), 
+                    labels = c("Manual", "Auntomático"))
+
+#scaterplot
+ggplot(mtcars, aes(x=hp, y=mpg))+geom_point()+
+  labs(x="caballos de fuerza",y="Millas por galon", 
+       title="Relación caballos de fuerza  y millas por galón")+
+  theme(panel.background = element_blank(), 
+        panel.grid.major = element_blank (),
+        panel.grid.minor = element_blank ())
+##
+ggplot(mtcars, aes(x=wt, y=hp))+geom_point()+
+  labs(x="peso",y="potencia", 
+       title="Relación peso-potencia")+
+  theme(panel.background = element_blank(), 
+        panel.grid.major = element_blank (),
+        panel.grid.minor = element_blank ())
+
+
+####Ejenplo de cater con 4 variables
+ggplot(mtcars, aes(x= hp, y=qsec))+ geom_point(aes(color= am, size= cyl))+
+  labs(x="caballos de fuerza", y="tiempo  en 1/4 de milla", 
+       title="Caballos-velocidad segun cilindraje  y tipo de caja")
--- Scripts/proyecto_economia_naranja.R
+++ Scripts/proyecto_economia_naranja.R
@@ -1,3 +1,6 @@
+library("plyr")
+library("ggplot2")
+library("dplyr")
 #definicion de la base de datos orange
 
 #Abrir archivo
@@ -57,3 +60,73 @@ plot(orangeec$GDP.PC ~ orangeec$Creat.Ind...GDP, xlab="Aporte economia Naranja
      al PIB(%)", 
      ylab="PIB per capita",
      main="Relacion economia naranja y pib per capita")
+
+#Histograma ejemplo
+
+ggplot()+geom_histogram(data = orangeec, aes(x=GDP.PC), fill = "Green", 
+                        color= "black", binwidth = 2000)+ 
+                        labs(x="pib per caíta", y="cantidad de paises",
+                             title="PIB per capita  pasises en latam")+
+  theme(legend.position = "none")+theme(panel.background = element_blank(), 
+                                        panel.grid.major = element_blank (),
+                                        panel.grid.minor = element_blank ())
+
+ggplot()+geom_histogram(data = orangeec, aes(x=Creat.Ind...GDP), fill = "Green", 
+                        color= "black", binwidth = 1)+ 
+  labs(x="Aporte economía naranja al pib% ", y="cantidad de paises",
+       title="Contribución economía naranja al pin en paises latam")+
+  theme(legend.position = "none")+theme(panel.background = element_blank(), 
+                                        panel.grid.major = element_blank (),
+                                        panel.grid.minor = element_blank ())
+
+
+  
+ggplot()+geom_histogram(data = orangeec, aes(x=Internet.penetration...population), fill = "Orange", 
+                        color= "yellow", binwidth = 5)+ 
+  scale_x_continuous(breaks = seq(40, max(100), 5))+
+  labs(x="Penetración Intenet como % población", y="cantidad de paises",
+       title="penetracion de intenet en paises latam")+
+  theme(legend.position = "none")+theme(panel.background = element_blank(), 
+                                        panel.grid.major = element_blank (),
+                                        panel.grid.minor = element_blank ())
+
+#
+(economy <- mean(orangeec$GDP.PC))
+
+#
+orangeec <- orangeec %>%
+  mutate(Stron_economy = ifelse(GDP.PC<economy,
+                                "Por debajo promedio pib per cápita",
+                                "Sobre-Arriba promeio pib per cápita"))
+View(orangeec)
+
+#boxplot
+
+ggplot(orangeec, aes(x=Stron_economy, y= Creat.Ind...GDP, fill=Stron_economy ))+
+  geom_boxplot(alpha=0.4)+
+  labs(x="tipo de pais", y="Aporte economía naranja alpib", 
+       title="Aporte economía naranja en pib paises latam  con alto y bajo
+       pib per cápita")+
+  theme(legend.position = "none")+theme(panel.background = element_blank(), 
+                                        panel.grid.major = element_blank (),
+                                        panel.grid.minor = element_blank ())
+
+
+##
+ggplot(orangeec, aes(x=Stron_economy, y=Internet.penetration...population ))+
+  geom_boxplot(alpha=0.8)+
+  labs(x="tipo de pais", y="Penetreación de intenet %", 
+       title="Penetración de Interner en  paises latam  con alto y bajo
+       pib per cápita")+
+  theme(legend.position = "none")+theme(panel.background = element_blank(), 
+                                        panel.grid.major = element_blank (),
+                                        panel.grid.minor = element_blank ())
+
+
+#scatterplot con ggplot
+ggplot(orangeec, aes(x= Internet.penetration...population, y=Creat.Ind...GDP))+
+  geom_point(aes(color= factor(Stron_economy), size= GDP.Growth..))+
+  labs(x="penetración Internet", y="Aporte economía naranja al Pib", 
+       title="Internet y aporte economía naranja según economí y crecimiento pib")
+
+
